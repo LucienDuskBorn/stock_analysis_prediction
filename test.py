f182a9103b4c866f07b2c3f738dca27ae598c13d
Lucien <1658727567@qq.com>
2025-09-11 22:22:30 +0800

    test.py测试数据时间修改

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import akshare as ak
 
-stock_us_hist_df = ak.stock_us_hist(symbol='MSFT', period="daily", start_date="20250901", end_date="20240910", adjust="qfq")
+stock_us_hist_df = ak.stock_us_hist(symbol='MSFT', period="daily", start_date="20250901", end_date="20250910", adjust="")
 print(stock_us_hist_df)
