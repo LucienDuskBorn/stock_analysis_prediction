828eceb240315d127ef1137cca8881a3cff264ea
nine <1658727567@qq.com>
2025-09-11 17:12:35 +0800

    test.py测试数据时间修改

--- test.py
+++ test.py
@@ -1,9 +1,4 @@
-from yahoofinancials import YahooFinancials
+import akshare as ak
 
-# 初始化对象
-yf = YahooFinancials('AAPL')
-start_date = "2025-06-01"
-end_date = "2025-06-20"
-# 获取历史收盘价
-historical_data = yf.get_historical_price_data(start_date, end_date, time_interval='daily')
-print(historical_data)
+stock_us_hist_df = ak.stock_us_hist(symbol='MSFT', period="daily", start_date="20250901", end_date="20240910", adjust="qfq")
+print(stock_us_hist_df)
